48dde8d321915324246263a1df402e82e8c253f0
douglastofoli <tofoli.douglas@hotmail.com>
2024-09-05 16:40:22 -0300

    add new tests for kv store

--- test/desafio_cli/kv_store_test.exs
+++ test/desafio_cli/kv_store_test.exs
@@ -4,31 +4,73 @@ defmodule DesafioCli.KVStoreTest do
   alias DesafioCli.KVStore
 
   setup do
-    KVStore.start_link([])
-    on_exit(fn -> KVStore.stop() end)
+    if File.exists?("kvstore_dets_test") do
+      File.rm!("kvstore_dets_test")
+    end
+
+    {:ok, _pid} = KVStore.start_link([], :kvstore_dets_test)
+    :ok
   end
 
-  test "set and get key-value" do
-    KVStore.set("name", "Alice")
-    assert KVStore.get("name") == "Alice"
+  describe "stop/0" do
+    test "close the dets file and stop the application" do
+      assert KVStore.stop() == :ok
+    end
   end
 
-  test "get returns :NIL for non-existent key" do
-    assert KVStore.get("age") == :NIL
+  describe "set/2" do
+    test "when it has a valid key and value, it stores the value in the key" do
+      assert KVStore.set("teste", 1) == {:FALSE, 1}
+      assert KVStore.set("teste", 2) == {:TRUE, 2}
+    end
   end
 
-  test "begin, rollback, and commit transactions" do
-    KVStore.set("name", "Alice")
-    KVStore.begin_transaction()
-    KVStore.set("name", "Bob")
-    assert KVStore.get("name") == "Bob"
+  describe "get/1" do
+    test "when the key exists, returns the value" do
+      KVStore.set("teste", 1)
 
-    KVStore.rollback_transaction()
-    assert KVStore.get("name") == "Alice"
+      assert KVStore.get("teste") == 1
+    end
 
-    KVStore.begin_transaction()
-    KVStore.set("name", "Charlie")
-    KVStore.commit_transaction()
-    assert KVStore.get("name") == "Charlie"
+    test "when the key does not exist, returns nil" do
+      assert KVStore.get("not_exist") == :NIL
+    end
+  end
+
+  describe "begin_transaction/0" do
+    test "when there is no transaction open, it opens and returns the value" do
+      assert KVStore.begin_transaction() == 1
+      assert KVStore.begin_transaction() == 2
+    end
+  end
+
+  describe "rollback_transaction/0" do
+    test "when there is an open transaction, undo the changes" do
+      # 1
+      KVStore.begin_transaction()
+
+      assert KVStore.rollback_transaction() == 0
+
+      # 1
+      KVStore.begin_transaction()
+      # 2
+      KVStore.begin_transaction()
+
+      assert KVStore.rollback_transaction() == 1
+    end
+
+    test "when there is no transaction open, an error is returned" do
+      assert KVStore.rollback_transaction() == {:error, "No transaction"}
+    end
+  end
+
+  describe "commit_transaction/0" do
+    test "when there is an open transaction, save the changes" do
+      assert KVStore.set("teste", 1) == {:FALSE, 1}
+      assert KVStore.begin_transaction() == 1
+      assert KVStore.set("teste", 2) == {:TRUE, 2}
+      assert KVStore.commit_transaction() == 0
+      assert KVStore.get("teste") == 2
+    end
   end
 end
--- test/desafio_cli_test.exs
+++ test/desafio_cli_test.exs
@@ -1,8 +1,19 @@
 defmodule DesafioCliTest do
   use ExUnit.Case
-  doctest DesafioCli
 
-  test "greets the world" do
-    assert DesafioCli.hello() == :world
+  import ExUnit.CaptureIO
+
+  test "main/1" do
+    input = """
+    EXIT
+    """
+
+    output =
+      capture_io([input: input, capture_prompt: false], fn ->
+        DesafioCli.main([])
+      end)
+
+    assert output =~ "Interactive KV Store started."
+    assert output =~ "Exiting program."
   end
 end
